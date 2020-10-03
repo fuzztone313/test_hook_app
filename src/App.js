02527d3cb0bdd8af474a674bbf9c1330333e249f
Norman <normanramadhan3@gmail.com>
2020-10-03 09:22:38 +0700

    Transform to cyberpunk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+import Logo from './assets/img/logo.png'
 
 function App() {
   return (
+    <>
+    <div className="image-wrapper">
+      <img className="image-content" src={Logo} alt="Logo" />
+    </div>
     <div className="todo-app">
       <TodoList />
     </div>
+    </>
   );
 }
 
