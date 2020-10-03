2deef0396f68c614f6b4de97daad1c350c0127b4
Norman <normanramadhan3@gmail.com>
2020-10-03 09:54:14 +0700

    Adding glitch fx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import * as VFX from 'react-vfx';
+import { VFXImg } from 'react-vfx';
 import './App.css';
 import TodoList from './components/TodoList';
-import Logo from './assets/img/logo.png'
+import Logo from './assets/img/logo.svg'
 
 function App() {
   return (
-    <>
+    <VFX.VFXProvider>
     <div className="image-wrapper">
-      <img className="image-content" src={Logo} alt="Logo" />
+    <VFXImg
+      className="image-content"
+      src={Logo}
+      shader="rgbGlitch"/>
     </div>
     <div className="todo-app">
       <TodoList />
     </div>
-    </>
+    </VFX.VFXProvider>
   );
 }
 
